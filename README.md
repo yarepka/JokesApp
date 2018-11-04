48eeebf2b62f8bb268a1eded2cd9db20d25b25f2
yarepka <yarepka01@gmail.com>
2018-11-04 13:30:34 +0300

    Bean configuration using XML example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JokesApp
-Java Bean Configuration, using @Configuration and @Bean annotations.
+Bean Configuration using XML configuration file.
+
+<b>@ImportResource("classpath:chuck-config.xml")</b> is used to import .xml configuration file, so the beans can be injected.
--- src/main/java/guru/springframework/jokes/jokesapp/JokesappApplication.java
+++ src/main/java/guru/springframework/jokes/jokesapp/JokesappApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.jokes.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesappApplication {
 
     public static void main(String[] args) {
